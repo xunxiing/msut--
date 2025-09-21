84b86eef1680cd14f03c55260f294f32956f7dae
xunxi <beijinghxn20081218@qq.com>
2025-09-21 20:29:50 +0800

    fix email字段问题

--- melon-tech-web/src/views/Dashboard.vue
+++ melon-tech-web/src/views/Dashboard.vue
@@ -3,7 +3,7 @@
     <el-main>
       <el-card>
         <template #header>控制台</template>
-        <p>欢迎，{{ auth.user?.name }}（{{ auth.user?.email }}）</p>
+        <p>欢迎，{{ auth.user?.name }}</p>
         <el-space>
           <el-button type="primary" @click="ping">测试受保护接口</el-button>
           <el-button @click="onLogout">退出登录</el-button>
